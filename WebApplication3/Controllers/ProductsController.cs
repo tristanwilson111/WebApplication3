386ddbf9b0f9ac728d7157beef63c0dfb107b97f
trist <tristanwilson111@gmail.com>
2017-09-20 15:23:16 -0400

    init commit

---
+++ WebApplication3/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(string products)
+        {
+            ViewBag.Product = products;
+
+            return View();
+        }
+    }
+}
